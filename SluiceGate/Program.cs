d71f2252ad0ef17626813a8cd11624fe29ee1e9c
MrBlubke <ward@impesoft.com>
2021-01-05 03:33:57 +0100

    readability

--- SluiceGate/Program.cs
+++ SluiceGate/Program.cs
@@ -13,7 +13,8 @@ namespace SluiceGate
             Console.Clear();
             foreach (Ship ship in GlobalVar.ShipList)
             {
-                Console.WriteLine($"arrived and added in sluice at {ship.ArrivalTime} name:{ship.Name} length:{(int)ship.Length * 30}m going {(ship.IsUpstream ? "up" : "down")}");
+                Console.WriteLine($"arrived and added in sluice at {ship.ArrivalTime}" +
+                    $" name:{ship.Name} length:{(int)ship.Length * 30}m going {(ship.IsUpstream ? "up" : "down")}");
             }
 
 
